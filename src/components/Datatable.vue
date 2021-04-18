73545f7a8cb2016f9e2d7a5130731cb52bd28430
ssamilg <samil-98@hotmail.com>
2021-04-18 19:18:48 +0300

    custom datatable component added

---
+++ src/components/Datatable.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'Datatable',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    headers: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+    <table class="ui celled table">
+      <thead>
+        <tr>
+          <th v-for="header in headers" :key=header>
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <template v-for="header in headers">
+            <td :key="header" :data-label="header">
+              {{ item[header] }}
+            </td>
+          </template>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<style>
+
+</style>
