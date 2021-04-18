5ccfec213870d02c336a096d98e8db36d7eb8ae2
ssamilg <samil-98@hotmail.com>
2021-04-18 19:42:51 +0300

    axios default setting made

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import axios from 'axios';
 import store from './store';
+import router from './router';
+import App from './App.vue';
 import 'fomantic-ui-css/semantic.min.css';
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
 
 new Vue({
   router,
