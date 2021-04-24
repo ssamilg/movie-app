8cd7f62fb60aed1aa11a809e46b45b8e0b8e5e6a
ssamilg <samil-98@hotmail.com>
2021-04-24 13:41:40 +0300

    snackbar class improvements

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -16,34 +16,12 @@ export default {
       this.$emit('close');
     }, 4000);
   },
-  computed: {
-    cardStyle() {
-      let style = '';
-
-      switch (this.type) {
-        case 'warning':
-          style = 'border: 1px solid #dda703; background-color: #fbbd08;';
-          break;
-        case 'error':
-          style = 'border: 1px solid #c02121; background-color: #DB2828;';
-          break;
-        case 'success':
-          style = 'border: 1px solid #1c9c3a; background-color: #21BA45;';
-          break;
-
-        default:
-          break;
-      }
-
-      return style;
-    },
-  },
 };
 </script>
 
 <template>
   <div id="snackbar">
-    <div class="ui card" :style="cardStyle">
+    <div class="ui card" :class="type">
       <div class="content">
         <div class="header">
           {{ text }}
@@ -53,7 +31,7 @@ export default {
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #snackbar {
   position: absolute;
   padding: 16px;
@@ -62,6 +40,21 @@ export default {
   bottom: 3%;
   right: 2%;
   animation: fadeIn .5s;
+
+  .warning {
+    border: 1px solid #dda703;
+    background-color: #fbbd08;
+  }
+
+  .error {
+    border: 1px solid #c02121;
+    background-color: #DB2828;
+  }
+
+  .success {
+    border: 1px solid #1c9c3a;
+    background-color: #21BA45;
+  }
 }
 
 @keyframes fadeIn {
